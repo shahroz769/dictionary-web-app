3a17cdc4d550f165564e55fa0c1a94def4fd9e8d
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2023-05-07 01:15:45 +0500

    UI fixes

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 function changeFont(font) {
     let bodyFont = document.getElementsByTagName('body')[0]
     let inputFont = document.getElementById('inputText')
+    let fontName = document.getElementById('fontName')
+    console.log(fontName)
     if (font === "sansSerif") {
         bodyFont.style.fontFamily = "Inter, sans-serif"
         inputFont.style.fontFamily = "Inter, sans-serif"
+        fontName.style.fontFamily = "Inter, sans-serif"
+        fontName.textContent = "Sans-Serif"
+
     } else if (font === "serif") {
         bodyFont.style.fontFamily = "Lora, serif"
         inputFont.style.fontFamily = "Lora, serif"
+        fontName.style.fontFamily = "Lora, serif"
+        fontName.textContent = "Serif"
     } else if (font === "mono") {
         bodyFont.style.fontFamily = "Inconsolata, mono"
         inputFont.style.fontFamily = "Inconsolata, mono"
+        fontName.style.fontFamily = "Inconsolata, mono"
+        fontName.textContent = "Mono"
     }
 }
