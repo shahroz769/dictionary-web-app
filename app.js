f78239b1afa4f7636f31b4e51f3bc48007e22913
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2023-05-07 20:20:05 +0500

    Completed

--- app.js
+++ app.js
@@ -32,24 +32,125 @@ function search() {
     let phoneticText = document.getElementById('phoneticText')
     let audio = document.getElementById('audio')
     let audioSrc = document.getElementById('audioSrc')
+    let sourceUrl = document.getElementById('sourceUrl')
+    let sourceUrlImg = document.getElementById('sourceUrlImg')
+    let definitionOne = document.getElementById('definitionOne')
+    let definitionTwo = document.getElementById('definitionTwo')
+    let definitionThree = document.getElementById('definitionThree')
+    let definitionFour = document.getElementById('definitionFour')
+    let example = document.getElementById('example')
+    let synonyms = document.getElementById('synonyms')
+    let synDiv = document.getElementById('syn-div')
+    let verb = document.getElementById('verb')
+    let allContent = document.getElementById('allcontent')
+    let contentDefault = document.getElementById('default')
+    let pandaError = document.getElementById('error')
+    let errorMsg = document.getElementById('errormsg')
     axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${userInput}`)
         .then(function (response) {
             console.log(response.data);
+            allContent.style.display = "block"
+            contentDefault.style.display = "none"
+            pandaError.style.display = "none"
+            errorMsg.style.display = "none"
             wordText.innerHTML = response.data[0].word
-            phoneticText.innerHTML = response.data[0].phonetics[0].text
-            audio.src = response.data[0].phonetics[0].audio
-            if (response.data[0].phonetics[0].audio) {
-                audio.src = response.data[0].phonetics[0].audio;
-            } else if (response.data[0].phonetics[1] && response.data[0].phonetics[1].audio) {
-                audio.src = response.data[0].phonetics[1].audio;
-            } else if (response.data[0].phonetics[2] && response.data[0].phonetics[2].audio) {
-                audio.src = response.data[0].phonetics[2].audio;
+            console.log("Error nhi Aya hai...")
+            if (response.data[0].phonetics.length == 0) {
+                phoneticText.style.display = "none"
             } else {
-                console.log('No audio URLs found in the response data.');
+                if (response.data[0].phonetics[0].text) {
+                    phoneticText.innerHTML = response.data[0].phonetics[0].text
+                    phoneticText.style.display = "block"
+                } else if (!response.data[0].phonetics[0].text) {
+                    phoneticText.style.display = "none"
+                } else if (response.data[0].phonetics[1].text) {
+                    phoneticText.innerHTML = response.data[0].phonetics[1].text
+                    phoneticText.style.display = "block"
+                } else {
+                    phoneticText.style.display = "none"
+                }
             }
+            console.log("Error nhi Aya hai...")
+            if (response.data[0].phonetics.length == 0) {
+                audioImg.style.display = "none"
+            } else {
+                audio.src = response.data[0].phonetics[0].audio
+                audioImg.style.display = "block"
+                audio.src = response.data[0].phonetics[0].audio
+                if (response.data[0].phonetics[0].audio) {
+                    audio.src = response.data[0].phonetics[0].audio;
+                } else if (response.data[0].phonetics[1] && response.data[0].phonetics[1].audio) {
+                    audio.src = response.data[0].phonetics[1].audio;
+                } else if (response.data[0].phonetics[2] && response.data[0].phonetics[2].audio) {
+                    audio.src = response.data[0].phonetics[2].audio;
+                } else if (response.data[0].phonetics[3] && response.data[0].phonetics[3].audio) {
+                    audio.src = response.data[0].phonetics[2].audio;
+                } else if (response.data[0].phonetics[4] && response.data[0].phonetics[4].audio) {
+                    audio.src = response.data[0].phonetics[2].audio;
+                } else if (response.data[0].phonetics[5] && response.data[0].phonetics[5].audio) {
+                    audio.src = response.data[0].phonetics[2].audio;
+                } else {
+                    console.log('No audio URLs found in the response data.');
+                }
+            }
+            console.log("Error nhi Aya hai...")
+            let length = response.data[0].meanings[0].definitions.length
+            if (length == 1) {
+                definitionOne.style.display = "block"
+                definitionTwo.style.display = "none"
+                definitionThree.style.display = "none"
+                definitionOne.innerHTML = response.data[0].meanings[0].definitions[0].definition
+            } else if (length == 2) {
+                definitionOne.style.display = "block"
+                definitionTwo.style.display = "block"
+                definitionThree.style.display = "none"
+                definitionOne.innerHTML = response.data[0].meanings[0].definitions[0].definition
+                definitionTwo.innerHTML = response.data[0].meanings[0].definitions[1].definition
+            } else if (length >= 3) {
+                definitionOne.style.display = "block"
+                definitionTwo.style.display = "block"
+                definitionThree.style.display = "block"
+                definitionOne.innerHTML = response.data[0].meanings[0].definitions[0].definition
+                definitionTwo.innerHTML = response.data[0].meanings[0].definitions[1].definition
+                definitionThree.innerHTML = response.data[0].meanings[0].definitions[2].definition
+            }
+            if (response.data[0].meanings[0].synonyms.length != 0) {
+                let syn = response.data[0].meanings[0].synonyms
+                let uppersyn = []
+                for (let i = 0; i < syn.length; i++) {
+                    let modifiedWord = syn[i][0].toUpperCase() + syn[i].slice(1).toLowerCase();
+                    uppersyn.push(modifiedWord);
+                }
+                synonyms.innerHTML = uppersyn.join(', ')
+                synDiv.style.display = "flex"
+            } else {
+                synDiv.style.display = "none"
+            }
+            if (response.data[0].meanings[0].partOfSpeech == "verb") {
+                if (response.data[0].meanings[0]) {
+                    definitionFour.innerHTML = response.data[0].meanings[0].definitions[0].definition
+                    verb.style.display = "block"
+                } else {
+                    verb.style.display = "none"
+                }
+            } else {
+                if (response.data[0].meanings[1]) {
+                    definitionFour.innerHTML = response.data[0].meanings[1].definitions[0].definition
+                    verb.style.display = "block"
+                } else {
+                    verb.style.display = "none"
+                }
+            }
+            sourceUrl.innerHTML = response.data[0].sourceUrls[0]
+            sourceUrl.href = response.data[0].sourceUrls[0]
+            sourceUrlImg.href = response.data[0].sourceUrls[0]
         })
         .catch(function (error) {
             console.log(error.data);
+            allContent.style.display = "none"
+            contentDefault.style.display = "none"
+            pandaError.style.display = "block"
+            errorMsg.style.display = "block"
         })
 }
 
