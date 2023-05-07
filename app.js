26f0a737984bac4040f8c66b0672226839a383a3
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2023-05-07 15:45:10 +0500

    Added Audio

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+function toggleTheme() {
+    let element = document.body;
+    element.classList.toggle("dark_theme");
+}
+
 function changeFont(font) {
     let bodyFont = document.getElementsByTagName('body')[0]
     let inputFont = document.getElementById('inputText')
@@ -20,7 +25,37 @@ function changeFont(font) {
         fontName.textContent = "Mono"
     }
 }
-function toggleTheme() {
-    let element = document.body;
-    element.classList.toggle("dark_theme");
+
+function search() {
+    let userInput = document.getElementById('inputText').value
+    let wordText = document.getElementById('wordText')
+    let phoneticText = document.getElementById('phoneticText')
+    let audio = document.getElementById('audio')
+    let audioSrc = document.getElementById('audioSrc')
+    axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${userInput}`)
+        .then(function (response) {
+            console.log(response.data);
+            wordText.innerHTML = response.data[0].word
+            phoneticText.innerHTML = response.data[0].phonetics[0].text
+            audio.src = response.data[0].phonetics[0].audio
+            if (response.data[0].phonetics[0].audio) {
+                audio.src = response.data[0].phonetics[0].audio;
+            } else if (response.data[0].phonetics[1] && response.data[0].phonetics[1].audio) {
+                audio.src = response.data[0].phonetics[1].audio;
+            } else if (response.data[0].phonetics[2] && response.data[0].phonetics[2].audio) {
+                audio.src = response.data[0].phonetics[2].audio;
+            } else {
+                console.log('No audio URLs found in the response data.');
+            }
+        })
+        .catch(function (error) {
+            console.log(error.data);
+        })
+}
+
+function playAudio() {
+    let audio = document.getElementById('audio')
+    if (audio.paused || audio.ended) {
+        audio.play();
+    }
 }
