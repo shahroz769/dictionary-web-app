e7b45852ee9aa8fd422fd192756a7c457f4eae1d
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2023-05-08 22:44:52 +0500

    Added button instead of search icon

--- app.js
+++ app.js
@@ -7,20 +7,24 @@ function changeFont(font) {
     let bodyFont = document.getElementsByTagName('body')[0]
     let inputFont = document.getElementById('inputText')
     let fontName = document.getElementById('fontName')
+    let buttonFont = document.getElementById('button')
     if (font === "sansSerif") {
         bodyFont.style.fontFamily = "Inter, sans-serif"
         inputFont.style.fontFamily = "Inter, sans-serif"
         fontName.style.fontFamily = "Inter, sans-serif"
+        buttonFont.style.fontFamily = "Inter, sans-serif"
         fontName.textContent = "Sans-Serif"
     } else if (font === "serif") {
         bodyFont.style.fontFamily = "Lora, serif"
         inputFont.style.fontFamily = "Lora, serif"
         fontName.style.fontFamily = "Lora, serif"
+        buttonFont.style.fontFamily = "Lora, serif"
         fontName.textContent = "Serif"
     } else if (font === "mono") {
         bodyFont.style.fontFamily = "Space Mono, mono"
         inputFont.style.fontFamily = "Space Mono, mono"
         fontName.style.fontFamily = "Space Mono, mono"
+        buttonFont.style.fontFamily = "Space Mono, mono"
         fontName.textContent = "Mono"
     }
 }
